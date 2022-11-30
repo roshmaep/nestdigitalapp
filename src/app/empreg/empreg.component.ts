ef91ba7fd46b6cacbd9ed1787605f77cff8e3686
roshmaep <roshmaep1999@gmail.com>
2022-12-01 00:27:59 +0530

    auth

--- src/app/empreg/empreg.component.ts
+++ src/app/empreg/empreg.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-empreg',
@@ -6,5 +7,99 @@ import { Component } from '@angular/core';
   styleUrls: ['./empreg.component.css']
 })
 export class EmpregComponent {
+  employeeid=""
+  firstname=""
+  lastname=""
+  housenumber=""
+  housename=""
+  street=""
+  pincode=""
+  district=""
+  state=""
+  country=""
+  mobilenumber=""
+  email=""
+  parentname=""
+  gender=""
+  degree=""
+  year=""
+  dof=""
+  username=""
+  password=""
+  confirmpassword=""
+constructor(private route:Router){}
+readValues=()=>{
+let data:any={
 
+  "employeeid":this.employeeid,
+
+  "firstname":this.firstname,
+
+  "lastname":this.lastname,
+
+  "houseno":this.housenumber,
+  "housename":this.housename,
+
+  "streetname":this.street,
+
+  "pincode":this.pincode,
+
+  "district":this.district,
+
+  "state":this.state,
+
+  "country":this.country,
+
+  "mobile":this.mobilenumber,
+
+  "email":this.email,
+
+  "parentname":this.parentname,
+
+  "gender":this.gender,
+
+  "highestdegree":this.degree,
+
+  "yearofexperience":this.year,
+
+  "dateofjoining":this.dof,
+
+  "username":this.username,
+
+  "password":this.password,
+
+  "confirmpassword":this.confirmpassword
+
+}
+console.log(data)
+if(this.confirmpassword==this.password) {
+console.log(data)
+  alert("Successfully registered")
+this.country
+this.confirmpassword
+this.degree
+this.district
+this.dof
+this.email
+this.employeeid
+this.firstname
+this.gender
+this.housename
+this.housenumber
+this.lastname
+this.mobilenumber
+this.parentname
+this.password
+this.pincode
+this.street
+this.state
+this.year
+this.username
+
+} else {
+
+  alert("Password and confirm password doesnot match")
+
+}
+}
 }
